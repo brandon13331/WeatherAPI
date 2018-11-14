b6da9b4ed2f7efb17192f5eb79d553a6226e0b16
brandon13331 <brandon13331@gmail.com>
2018-11-13 17:35:18 -0800

    Added web page (URL) reader

---
+++ src/main/model/ReadWebPage.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ReadWebPage {
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+
+        BufferedReader br = null;
+
+        try {
+            String apikey = ""; // API key
+            String query = "https://api.openweathermap.org/";
+            String theURL = query + apikey;
+            URL url = new URL(theURL);
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line;
+
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+
+            System.out.println(sb);
+        } finally {
+
+            if (br != null) {
+                br.close();
+            }
+        }
+    }
+}
